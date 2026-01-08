6eb258d63fd9fd38ab9f38b730ae4c8ab736deb4
Loris <loris.mischler@epitech.eu>
2026-01-08 20:45:17 +0100

    fix

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,9 +1,12 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   devtools: { enabled: true },
-  
+
   // App configuration
   app: {
+    // Étape 1 : indispensable pour GitHub Pages
+    baseURL: '/portfolio/',
+
     head: {
       charset: 'utf-8',
       viewport: 'width=device-width, initial-scale=1',
